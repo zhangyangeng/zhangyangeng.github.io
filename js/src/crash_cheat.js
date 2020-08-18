202336812638f5a10188abe786c27780a6f6355c
zhangyangeng <15035374951@126.com>
2020-08-18 18:42:59 +0800

    Site updated: 2020-08-18 18:42:40

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Sat Aug 15 2020 18:50:49 GMT+0800 (GMT+08:00)
+// build time:Tue Aug 18 2020 18:42:36 GMT+0800 (GMT+08:00)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="╭(°A°`)╮ 页面崩溃啦 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(ฅ>ω<*ฅ) 噫又好啦 ~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
