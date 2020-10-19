37c9b27988d355fe19117d92f52990d31a95598c
zhangyangeng <15035374951@126.com>
2020-10-19 16:50:41 +0800

    Site updated: 2020-10-19 16:50:16

--- demo/JSex/js/bomButtonBoxPlusPlus.js
+++ demo/JSex/js/bomButtonBoxPlusPlus.js
@@ -1,2 +1,41 @@
-// build time:Mon Oct 19 2020 16:44:44 GMT+0800 (GMT+08:00)
-function move(e,t,r,n,l){clearInterval(e.timer);var i=parseInt(getStyle(e,t));if(i>r){n=-n}e.timer=setInterval(function(){var i=parseInt(getStyle(e,t));var a=i+n;if(n>0&&a>r||n
+//创建一个函数用来控制盒子移动动画
+/*
+ * 参数：
+ *   1.元素名obj
+ *   2.目标位置target
+ *   3.速度speed
+ *   4.属性attr
+ *   5.回调函数callback
+ */
+/* var timer;
+//注意：当timer定义在全局变量中时，两个元素调用时就会造成冲突，所以不能这样定义，应该使用元素.timer来定义 */
+function move(obj, attr, target, speed, callback) {
+	clearInterval(obj.timer);
+	//自动判断speed正负值，减少用户计算过程。这里获取当前位置
+	var current = parseInt(getStyle(obj, attr));
+	if (current > target) {
+		speed = -speed;
+	}
+	obj.timer = setInterval(function() {
+		var oldValue = parseInt(getStyle(obj, attr));
+		var newValue = oldValue + speed;
+		//当speed为负值时向左移，界限为小于target；当speed为正值时向右移，界限为大于target
+		if ((speed > 0 && newValue > target) || (speed < 0 && newValue < target)) {
+			newValue = target;
+		}
+		obj.style[attr] = newValue + "px";
+		if (newValue == target) {
+			clearInterval(obj.timer);
+			//当结束完此次动画后如果有回调函数就调用回调函数，如果没有则不执行
+			callback && callback();
+		}
+	}, 50);
+}
+//创建一个函数用来获取元素的样式
+function getStyle(obj, name) {
+	if (window.getComputedStyle) {
+		return getComputedStyle(obj, null)[name];
+	} else {
+		return obj.currentStyle[name];
+	}
+}
