7c3d1210487cd98d09e3c5f7f01fbc66601aeb1e
zhangyangeng <15035374951@126.com>
2020-10-23 21:24:54 +0800

    Site updated: 2020-10-23 21:24:35

--- demo/JSex/js/tools.js
+++ demo/JSex/js/tools.js
@@ -39,3 +39,40 @@ function getStyle(obj, name) {
 		return obj.currentStyle[name];
 	}
 }
+
+
+//定义一个函数，来向一个元素中添加指定的class属性
+/*
+ * 参数：
+ * obj 要添加的class属性
+ * cn 要添加的class值
+ */
+function addClass(obj, cn) {
+	//检查obj中是否含有cn，有的时候就不管，没有就添加
+	if (!hasClass(obj, cn)) {
+		obj.className += " " + cn;
+	}
+}
+//定义一个函数，判断一个元素中是否含有指定的class属性
+function hasClass(obj, cn) {
+	//创建一个正则表达式，因为这里是传obj不需要写死，所以需要构造函数的写法
+	//有时候class属性有很多个，所以需要使用 \b 来设置字符边界
+	var reg = new RegExp("\\b" + cn + "\\b");
+	//使用test()方法来检测其中是否有cn
+	return reg.test(obj.className);
+}
+//定义一个函数，删除一个元素中指定的class属性
+function removeClass(obj, cn) {
+	//创建一个正则表达式
+	var reg = new RegExp("\\b" + cn + "\\b");
+	//删除class，即用空串替换掉原本的class属性
+	obj.className = obj.className.replace(reg, "");
+}
+//定义一个函数，可以切换一个类，即有则删除，无则添加
+function toggleClass(obj, cn) {
+	if (hasClass(obj, cn)) {
+		removeClass(obj, cn);
+	} else {
+		addClass(obj, cn);
+	}
+}
