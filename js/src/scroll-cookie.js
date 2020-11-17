f08e20e5fb9db908706d85f295b904d2c13f3015
zhangyangeng <15035374951@126.com>
2020-11-17 17:08:25 +0800

    Site updated: 2020-11-17 17:08:02

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Nov 12 2020 22:06:20 GMT+0800 (GMT+08:00)
+// build time:Tue Nov 17 2020 17:07:57 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
