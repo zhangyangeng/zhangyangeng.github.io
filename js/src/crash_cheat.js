07f597f3eb7e27688753b3150ea8d66c3614540d
zhangyangeng <15035374951@126.com>
2021-01-19 19:52:39 +0800

    Site updated: 2021-01-19 19:52:19

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Thu Jan 14 2021 22:27:59 GMT+0800 (GMT+08:00)
+// build time:Tue Jan 19 2021 19:52:13 GMT+0800 (GMT+08:00)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="╭(°A°`)╮ 页面崩溃啦 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(ฅ>ω<*ฅ) 噫又好啦 ~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
